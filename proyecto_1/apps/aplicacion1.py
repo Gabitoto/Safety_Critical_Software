bae23ee0445a5d8354ecf8347f5a5f8240129bf4
Lucas G <133524925+Gabitoto@users.noreply.github.com>
2025-04-22 12:44:49 -0300

    agrego primer programa de prueba

--- proyecto_1/apps/aplicacion1.py
+++ proyecto_1/apps/aplicacion1.py
@@ -1,1 +1,22 @@
 # Aplicación secundaria
+
+def suma(a, b):
+    # Precondición: a y b deben ser números
+    assert type(a) in (int, float) and type(b) in (int, float)
+    
+    # Calcular la suma
+    resultado = a + b
+    
+    # Postcondición: El resultado debe ser un número
+    assert type(resultado) in (int, float)
+    
+    return resultado
+
+# Ejemplo de uso
+resultado = suma(2, 3)
+print(resultado) # Salida: 5
+
+# Ejemplo de error
+# Intentar sumar un número y una cadena
+# La sentencia assert generará un error
+# resultado = suma(2, "3")
